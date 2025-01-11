fbd4797b35254276a8fd4b640436457afac84102
Nathan Humphrey <nathpaulhump@gmail.com>
2025-01-10 18:01:04 -0700

    Initial week 01 project

---
+++ week_01_dart_demo/bin/week_01_dart_demo.dart
@@ -0,0 +1,5 @@
+import 'package:week_01_dart_demo/week_01_dart_demo.dart';
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculate()}!');
+}
---
+++ week_01_dart_demo/test/week_01_dart_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:week_01_dart_demo/week_01_dart_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
