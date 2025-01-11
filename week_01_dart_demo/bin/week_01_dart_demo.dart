95ebf1f44488d70f7dbdc453c22dfab9155d4a90
Nathan Humphrey <nathpaulhump@gmail.com>
2025-01-10 18:31:19 -0700

    Week 01 basic input

--- week_01_dart_demo/bin/week_01_dart_demo.dart
+++ week_01_dart_demo/bin/week_01_dart_demo.dart
@@ -1,5 +1,19 @@
-import 'package:week_01_dart_demo/week_01_dart_demo.dart';
+import 'dart:io';
+// import 'package:week_01_dart_demo/week_01_dart_demo.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${calculate()}!');
+  // Initialized variables do not require type
+  // const name = 'Sally';
+  // final age = calculate();
+
+  String name;
+  int age;
+
+  stdout.write('Enter your name: ');
+  name = stdin.readLineSync()!;
+
+  stdout.write('Enter your age: ');
+  age = int.parse(stdin.readLineSync()!);
+
+  stdout.writeln('Hello $name, it looks like you are $age years old.');
 }
