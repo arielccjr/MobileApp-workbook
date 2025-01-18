3078ad0a66cb606c9d237e62e7cbcb2352cc3b1c
Nathan Humphrey <nathpaulhump@gmail.com>
2025-01-17 17:40:46 -0700

    Week 02 Profile start

---
+++ week_02_profile_demo/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile'),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
