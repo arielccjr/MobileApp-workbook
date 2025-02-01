cad5e7f05c82436fa77b56efc81d502715fc6289
Nathan Humphrey <nathpaulhump@gmail.com>
2025-01-31 17:51:04 -0700

    Week 04 Form Widget

--- week_04_forms_demo/lib/main.dart
+++ week_04_forms_demo/lib/main.dart
@@ -16,37 +16,53 @@ class MainApp extends StatelessWidget {
         ),
         body: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const Text('New User Form'),
-              const TextField(
-                decoration: InputDecoration(
-                  label: Text('Username'),
-                ),
-              ),
-              const TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  label: Text('Password'),
-                ),
-              ),
-              // You can use SizedBox for layout
-              // SizedBox(
-              //   height: 16.0,
-              // ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Theme.of(context).colorScheme.primary,
-                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                ),
-                onPressed: () {},
-                child: const Text('Sign Up'),
-              ),
-            ],
-          ),
+          child: UserSignUpForm(),
         ),
+      ),
+    );
+  }
+}
+
+class UserSignUpForm extends StatelessWidget {
+  const UserSignUpForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          const Text('New User Form'),
+          TextFormField(
+            validator: (value) => value == null || value.isEmpty
+                ? 'Username cannot be empty'
+                : null,
+            decoration: InputDecoration(
+              label: Text('Username'),
+            ),
+          ),
+          TextFormField(
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(
+              label: Text('Password'),
+            ),
+          ),
+          // You can use SizedBox for layout
+          // SizedBox(
+          //   height: 16.0,
+          // ),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+            ),
+            onPressed: () {
+              // Get the text field value ... ?
+            },
+            child: const Text('Sign Up'),
+          ),
+        ],
       ),
     );
   }
