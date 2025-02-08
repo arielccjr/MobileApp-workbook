0bb9aa2cfdbb6a2f63fb001cc4a3fbc1eb085ff5
Nathan Humphrey <nathpaulhump@gmail.com>
2025-02-07 17:21:07 -0700

    Week 05 Sub Page

---
+++ week_05_navigation_demo/lib/pages/sub_pages/sub.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SubPage extends StatelessWidget {
+  const SubPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Text('Main Sub Page'),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Sub 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Sub 2'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
