1290671054d3d35868557a02a4f113d6c96f2f85
Nathan Humphrey <nathpaulhump@gmail.com>
2025-02-12 17:49:25 -0700

    Week 06 SQLite getDatabase

---
+++ week_06_sqlite_storage/lib/services/db.dart
@@ -0,0 +1,23 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+const databaseName = 'todos.db';
+
+Future<Database> getDatabase() async {
+  var databasePath = await getDatabasesPath();
+
+  return openDatabase(
+    join(databasePath, databasePath),
+    version: 1,
+    onCreate: (db, version) async {
+      // When the database is created, creat the todos table
+      await db.execute('''
+          CREATE TABLE todos (
+            id INTEGER PRIMARY KEY,
+            description TEXT,
+            done INTEGER DEFAULT 0
+          )
+      ''');
+    },
+  );
+}
