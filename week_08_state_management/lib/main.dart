9e9f392957fdb291d4020a762982f2908b6aea3a
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-05 18:33:53 -0700

    Week 08 State Mgmt Provider

--- week_08_state_management/lib/main.dart
+++ week_08_state_management/lib/main.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:week_08_state_management/models/user.dart';
-import 'package:week_08_state_management/user_notifier.dart';
 
 import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => User('Sally', 'Anne'),
+      child: const MainApp(),
+    ),
+  );
 }
-
-User user = User('Jon', 'Doe');
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return UserNotifier(
-      user: user,
-      child: Builder(
-        builder: (BuildContext context) {
-          return MaterialApp(home: Scaffold(body: Center(child: HomePage())));
-        },
-      ),
-    );
+    return const MaterialApp(home: Scaffold(body: Center(child: HomePage())));
   }
 }
--- week_08_state_management/lib/pages/first_name_page.dart
+++ week_08_state_management/lib/pages/first_name_page.dart
@@ -1,36 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:week_08_state_management/models/user.dart';
-import 'package:week_08_state_management/user_notifier.dart';
 
 class FirstNamePage extends StatelessWidget {
   const FirstNamePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    User user = UserNotifier.of(context);
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('First Name Page')),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 24.0),
-            ListenableBuilder(
-              listenable: user,
-              builder:
-                  (context, child) =>
-                      Text('User name: ${user.firstName} ${user.lastName}'),
+    return Consumer<User>(
+      builder:
+          (context, user, child) => Scaffold(
+            appBar: AppBar(title: const Text('First Name Page')),
+            body: Center(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 24.0),
+                  Text('User name: ${user.firstName} ${user.lastName}'),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        // Update our user firs name here
-        onPressed: () {
-          user.firstName = 'NewFirstName';
-        },
-        child: const Icon(Icons.update),
-      ),
+            floatingActionButton: FloatingActionButton(
+              // Update our user firs name here
+              onPressed: () {
+                user.firstName = 'NewFirstName';
+              },
+              child: const Icon(Icons.update),
+            ),
+          ),
     );
   }
 }
