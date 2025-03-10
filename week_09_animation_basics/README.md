cb7d1c773a323108200f3f42a1dffac204f0da9f
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-10 17:22:58 -0600

    Week 09 Animation Basics Start

---
+++ week_09_animation_basics/README.md
@@ -0,0 +1,3 @@
+# week_09_animation_basics
+
+A new Flutter project.
---
+++ week_09_animation_basics/android/app/src/main/kotlin/com/example/week_09_animation_basics/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week_09_animation_basics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
