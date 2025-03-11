2d183bc58b19bc666394338ec600eb890ccc315e
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-10 18:13:56 -0600

    Week 09 Animation Controller

--- week_09_animation_basics/lib/animations.dart
+++ week_09_animation_basics/lib/animations.dart
@@ -2,8 +2,32 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
-class AnimationsPage extends StatelessWidget {
+class AnimationsPage extends StatefulWidget {
   const AnimationsPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AnimationsPageState();
+}
+
+class _AnimationsPageState extends State<AnimationsPage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    )..repeat();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
