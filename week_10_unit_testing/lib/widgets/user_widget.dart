e1b678f7f58b2643f3ac12c601297dcedc39c4a8
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-17 18:26:45 -0600

    Week 10 UserWidget

---
+++ week_10_unit_testing/lib/widgets/user_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:week_10_unit_testing/models/user.dart';
+
+class UserWidget extends StatelessWidget {
+  const UserWidget({required this.user, super.key});
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Text('Name: ${user.name}'), Text('Email: ${user.email}')],
+    );
+  }
+}
--- week_10_unit_testing/test/user_test.dart
+++ week_10_unit_testing/test/user_test.dart
@@ -4,17 +4,23 @@ import 'package:week_10_unit_testing/models/user.dart';
 main() {
   group('User Tests', () {
     late User user;
+    const name = 'Jane Doe';
+    const email = 'jdoe@example.com';
 
     setUp(() {
-      user = const User(name: 'Jane Doe', email: 'jdoe@example.com');
+      user = const User(name: name, email: email);
     });
 
     test('User should have a name', () {
-      expect(user.name, 'Jane Doe');
+      expect(user.name, name);
     });
 
     test('User should have an email', () {
-      expect(user.email, 'jdoe@example.com');
+      expect(user.email, email);
+    });
+
+    test('User toString() returns correct value', () {
+      expect(user.toString(), 'name: $name, email: $email');
     });
   });
 }
