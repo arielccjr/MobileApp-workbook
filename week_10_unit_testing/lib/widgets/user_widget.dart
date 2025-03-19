9d4a0ca18a776bd6474452e986cc6aaa8e83cecf
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-19 17:25:52 -0600

    Week 10 Stateful User Widget

--- week_10_unit_testing/lib/widgets/user_widget.dart
+++ week_10_unit_testing/lib/widgets/user_widget.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:week_10_unit_testing/models/user.dart';
 
-class UserWidget extends StatelessWidget {
+class UserWidget extends StatefulWidget {
   const UserWidget({required this.user, super.key});
 
   final User user;
 
   @override
+  State<UserWidget> createState() => _UserWidgetState();
+}
+
+class _UserWidgetState extends State<UserWidget> {
+  var isHidden = true;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Text('Name: ${user.name}'), Text('Mail: ${user.email}')],
+    return Scaffold(
+      appBar: AppBar(title: const Text('Testing Page')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            isHidden = !isHidden;
+          });
+        },
+      ),
+      body:
+          isHidden
+              ? const Text('User Info Hidden')
+              : Column(
+                children: [
+                  Text('Name: ${widget.user.name}'),
+                  Text('Mail: ${widget.user.email}'),
+                ],
+              ),
     );
   }
 }
